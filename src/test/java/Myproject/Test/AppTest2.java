9d4ef75c2b4fa7d3b6dae5a57520ed214dbbda19
shailendraraj <shailendraraj1167@gmail.com>
2024-03-04 21:20:36 +0530

    Final Commit

---
+++ src/test/java/Myproject/Test/AppTest2.java
@@ -0,0 +1,47 @@
+package Myproject.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest2 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	
+	/**
+     * Rigorous Test :-)
+     */
+	
+	/**
+     * Rigorous Test :-)
+     */
+	
+	/**
+     * Rigorous Test :-)
+     */
+	
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }
+    
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+}
